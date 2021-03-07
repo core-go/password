316474a6841db3680ae788e7f0be743bb7766b00
Duc Nguyen <minhduc140583@gmail.com>
2021-03-07 21:31:22 +0700

    Delete unused interfaces

--- password_change.go
+++ password_change.go
@@ -6,5 +6,5 @@ type PasswordChange struct {
 	Passcode        string `json:"passcode,omitempty" gorm:"column:passcode" bson:"passcode,omitempty" dynamodbav:"passcode,omitempty" firestore:"passcode,omitempty"`
 	CurrentPassword string `json:"currentPassword,omitempty" gorm:"column:currentpassword" bson:"currentPassword,omitempty" dynamodbav:"currentPassword,omitempty" firestore:"currentPassword,omitempty"`
 	Password        string `json:"password,omitempty" gorm:"column:password" bson:"password,omitempty" dynamodbav:"password,omitempty" firestore:"password,omitempty"`
-	SenderType      string `json:"senderType,omitempty" gorm:"column:sendertype" bson:"senderType,omitempty" dynamodbav:"senderType,omitempty" firestore:"senderType,omitempty"`
+	Sender          string `json:"sender,omitempty" gorm:"column:sender" bson:"sender,omitempty" dynamodbav:"sender,omitempty" firestore:"sender,omitempty"`
 }
--- two_factors_repository.go
+++
@@ -1,7 +0,0 @@
-package password
-
-import "context"
-
-type TwoFactorsRepository interface {
-	Require(ctx context.Context, id string) (bool, error)
-}
