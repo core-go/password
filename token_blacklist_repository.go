c6b2eefb836dba82be9002e49709be2d72763da2
Duc Nguyen <minhduc140583@gmail.com>
2021-03-07 21:27:46 +0700

    Change to functions

--- token_blacklist_repository.go
+++
@@ -1,9 +0,0 @@
-package password
-
-import "time"
-
-type TokenBlacklistRepository interface {
-	Revoke(token string, reason string, expires time.Time) error
-	RevokeAllTokens(id string, reason string) error
-	Check(id string, token string, createAt time.Time) string
-}
